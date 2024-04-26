952d6c4df278598f10f75a50c8241296a6ce2aec
Moldir <51477205+Moldir99@users.noreply.github.com>
2024-04-26 19:00:57 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function ingredientsHover(){
+				document.getElementById("ingredients").style.fontSize = "300%";
+			}
+			
+function ingredientsNormal(){
+				document.getElementById("ingredients").style.fontSize = "100%";
+			}
+			
+function preparationHover(){
+				document.getElementById("preparation").style.fontSize = "300%";
+			}
+			
+function preparationNormal(){
+				document.getElementById("preparation").style.fontSize = "100%";
+			}
+    
